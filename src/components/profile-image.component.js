6b78ec51b2b854b97e0c5e0582e1337c2d3dde53
Mohammad Zaini <Mohammad_zaini@outlook.com>
2023-06-04 20:24:15 +0300

    Enabling the user to set a profile picture

--- src/components/profile-image.component.js
+++ src/components/profile-image.component.js
@@ -1,21 +1,39 @@
-import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
 import userImage from "../../assets/images/userImage.jpeg"
 import { colors } from "../infrastructure/theme/colors";
 import { FontAwesome } from '@expo/vector-icons';
 import styled from "styled-components";
+import { launchImagePicker } from "./utils/image-picker-helper";
 
 export const ProfileImage = props => {
+    const source = props.uri ? { uri: props.uri } : userImage;
+
+    const [image, setImage] = useState(source);
+
+    const pickImage = async () => {
+        try {
+            const tempUri = await launchImagePicker();
+
+            if (!tempUri) return;
+
+            setImage({ uri: tempUri });
+
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
     return (
-        <View>
+        <TouchableOpacity onPress={pickImage} >
             <Image
                 style={{ ...styles.image, ...{ width: props.size, height: props.size } }}
-                source={userImage} />
+                source={image} />
 
             <IconContainer>
                 <FontAwesome name="pencil" size={20} color="black" />
             </IconContainer>
-        </View>
+        </TouchableOpacity>
     );
 };
 
